99cb2c0a4872bc3c00ef2fd856f477aebc986ac9
AustinHeinrich <austinwheinrich@gmail.com>
2020-03-25 16:00:33 -0600

    continued work on pactmaker, pushing to gitlab

--- src/main/java/kobolds/the_elder/blocks/PactmakerVital.java
+++ src/main/java/kobolds/the_elder/blocks/PactmakerVital.java
@@ -3,7 +3,10 @@ package kobolds.the_elder.blocks;
 import kobolds.the_elder.Elder;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -20,6 +23,14 @@ public class PactmakerVital extends Block {
     }
 
     @Override
-    public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (!worldIn.isRemote) {
+
+            // do stuff
+
+            return true;
+        }
+
+        return false;
     }
 }
