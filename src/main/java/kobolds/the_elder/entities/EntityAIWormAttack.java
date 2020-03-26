f58001788901f7fdc214777ff0654147c2b7488b
Haydn V <hvh552@usask.ca>
2020-03-25 19:55:23 -0600

    Made worm beeg

--- src/main/java/kobolds/the_elder/entities/EntityAIWormAttack.java
+++ src/main/java/kobolds/the_elder/entities/EntityAIWormAttack.java
@@ -2,11 +2,10 @@ package kobolds.the_elder.entities;
 
 import net.minecraft.entity.ai.EntityAIAttackMelee;
 
-public class EntityAITempAttack extends EntityAIAttackMelee {
-    private int raiseArmTicks;
-    private EntityTemp me;
+public class EntityAIWormAttack extends EntityAIAttackMelee {
+    private EntityWorm me;
 
-    public EntityAITempAttack(EntityTemp meIn, double speedIn, boolean longMemoryIn) {
+    public EntityAIWormAttack(EntityWorm meIn, double speedIn, boolean longMemoryIn) {
         super(meIn, speedIn, longMemoryIn);
         this.me = meIn;
     }
@@ -14,25 +13,15 @@ public class EntityAITempAttack extends EntityAIAttackMelee {
     @Override
     public void startExecuting() {
         super.startExecuting();
-        this.raiseArmTicks = 0;
     }
 
     @Override
     public void resetTask() {
         super.resetTask();
-        this.me.setArmsRaised(false);
     }
 
     @Override
     public void updateTask() {
         super.updateTask();
-        ++this.raiseArmTicks;
-
-        if (this.raiseArmTicks >= 5 && this.attackTick < 10) {
-            this.me.setArmsRaised(true);
-        }
-        else {
-            this.me.setArmsRaised(false);
-        }
     }
 }
