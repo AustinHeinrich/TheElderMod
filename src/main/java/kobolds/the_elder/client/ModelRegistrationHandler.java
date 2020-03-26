1a709ef52611e51299c33c2755a8adc3d209c293
AustinHeinrich <austinwheinrich@gmail.com>
2020-03-26 02:10:28 -0600

    making textures work

--- src/main/java/kobolds/the_elder/client/ModelRegistrationHandler.java
+++ src/main/java/kobolds/the_elder/client/ModelRegistrationHandler.java
@@ -1,11 +1,13 @@
 package kobolds.the_elder.client;
 
+import jdk.nashorn.internal.ir.Block;
 import kobolds.the_elder.init.ModBlocks;
 import kobolds.the_elder.init.ModItems;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -31,7 +33,6 @@ public class ModelRegistrationHandler {
     }
 
     private static void registerModel(Item item, int meta) {
-
         ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), "inventory"));
     }
 
