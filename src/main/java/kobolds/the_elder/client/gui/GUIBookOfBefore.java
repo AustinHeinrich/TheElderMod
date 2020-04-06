d3b03a1eaa7c42f7b6f185d6fd24c76ca2ced88b
AustinHeinrich <austinwheinrich@gmail.com>
2020-04-06 04:11:25 -0600

    now it makes sense why so many modders use apis for guidebooks, also more progress on arrows

--- src/main/java/kobolds/the_elder/client/gui/GUIBookOfBefore.java
+++ src/main/java/kobolds/the_elder/client/gui/GUIBookOfBefore.java
@@ -3,28 +3,30 @@ package kobolds.the_elder.client.gui;
 import kobolds.the_elder.Elder;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.GuiScreenBook;
+import net.minecraft.client.gui.*;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.RenderTooltipEvent;
+import org.lwjgl.input.Keyboard;
 
 import java.io.IOException;
 
 public class GUIBookOfBefore extends GuiScreen {
 
     private static final int BOOK_PAGES = 2;
+    private static final int BOOK_HEIGHT = 192;
+    private static final int BOOK_WIDTH = 192;
+
     private int currPage = 0;
-    private static final int GUI_HEIGHT = 192;
-    private static final int GUI_WIDTH = 192;
 
     private static ResourceLocation[] bookPageTexture = new ResourceLocation[BOOK_PAGES];
     private static String[] pageText = new String[BOOK_PAGES];
+    private GuiButton exitButton;
+    private GuiButton nextButton;
+    private GuiButton prevButton;
 
     public GUIBookOfBefore() {
         bookPageTexture[0] = new ResourceLocation(Elder.MODID, "textures/gui/book_of_before.png");
@@ -36,28 +38,47 @@ public class GUIBookOfBefore extends GuiScreen {
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        int centerY = (height / 2) - GUI_HEIGHT / 2;
-        int centerX = (width / 2) - GUI_WIDTH / 2;
+        int offsetFromScreenLeft = (width - BOOK_WIDTH) / 2;
 
         drawDefaultBackground(); // darken the background
 
         // book
         this.mc.renderEngine.bindTexture(bookPageTexture[currPage]);
-        drawTexturedModalRect(centerX, centerY, 0,0, GUI_HEIGHT, GUI_WIDTH);
+        drawTexturedModalRect(offsetFromScreenLeft, 2, 0,0, BOOK_HEIGHT, BOOK_WIDTH);
 
         // text
-        this.fontRenderer.drawSplitString(pageText[currPage], centerX+36, 36, 116,0x00000);
+        this.fontRenderer.drawSplitString(pageText[currPage],offsetFromScreenLeft + 36, 34, 116,0x00000);
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
     @Override
     public void initGui() {
-        super.initGui();
+        int offsetFromScreenLeft = (width - BOOK_WIDTH) / 2;
+
+        buttonList.clear();
+        Keyboard.enableRepeatEvents(true);
+
+        exitButton = new GuiButton(0, width / 2, 4 + BOOK_HEIGHT, 98, 20,
+                I18n.format("gui.done", new Object[0]));
+        buttonList.add(exitButton);
     }
 
+    // work the buttons
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
-        super.actionPerformed(button);
+        if (button == exitButton) {
+            mc.displayGuiScreen((GuiScreen) null);
+        }
+        else if (button == nextButton) {
+            if (currPage < BOOK_PAGES - 1) {
+                ++currPage;
+            }
+        }
+        else if (button == prevButton) {
+            if (currPage > 0) {
+                --currPage;
+            }
+        }
     }
 
     // gui will not pause game in single player
