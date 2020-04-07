89056ad9a2d8a895002bb9dd6316ee2c7f484742
AustinHeinrich <austinwheinrich@gmail.com>
2020-04-07 01:23:48 -0600

    stair item now appears, rather than unloaded texture in inventory

--- src/main/java/kobolds/the_elder/blocks/base/StairsBlockBase.java
+++ src/main/java/kobolds/the_elder/blocks/base/StairsBlockBase.java
@@ -3,11 +3,13 @@ package kobolds.the_elder.blocks.base;
 import kobolds.the_elder.Elder;
 import kobolds.the_elder.init.ModBlocks;
 import kobolds.the_elder.init.ModItems;
+import kobolds.the_elder.util.interfaces.IHasModel;
 import net.minecraft.block.BlockStairs;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
-public class StairsBlockBase extends BlockStairs {
+public class StairsBlockBase extends BlockStairs implements IHasModel {
 
     public StairsBlockBase(String name, IBlockState modelState) {
         super(modelState);
@@ -21,5 +23,10 @@ public class StairsBlockBase extends BlockStairs {
 
         ModBlocks.BLOCKS.add(this);
         ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+    }
+
+    @Override
+    public void registerModels() {
+        Elder.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
     }
 }
