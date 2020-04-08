25a3d85f2fdbebb7c4774f0e5bc22e5a29e76d1f
Lachlan <lcm824@usask.ca>
2020-04-07 19:00:45 -0600

    made portal work

---
+++ src/main/java/kobolds/the_elder/blocks/BlockElderPortalFrame.java
@@ -0,0 +1,121 @@
+package kobolds.the_elder.blocks;
+
+import com.google.common.base.Predicates;
+import kobolds.the_elder.Elder;
+import kobolds.the_elder.init.ModBlocks;
+import kobolds.the_elder.init.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.BlockWorldState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.state.pattern.BlockPattern;
+import net.minecraft.block.state.pattern.BlockStateMatcher;
+import net.minecraft.block.state.pattern.FactoryBlockPattern;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Random;
+
+public class BlockElderPortalFrame extends Block {
+    public static final PropertyBool ACTIVE = PropertyBool.create("active");
+
+    public BlockElderPortalFrame(String name)
+    {
+        super(Material.ROCK, MapColor.GREEN);
+
+        setRegistryName(name);
+        setUnlocalizedName(Elder.MODID + "." + name);
+        setCreativeTab(Elder.ELDER_TAB);
+
+        ModBlocks.BLOCKS.add(this);
+        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+
+        this.setDefaultState(this.blockState.getBaseState().withProperty(ACTIVE, false));
+    }
+
+    /**
+     * Called by ItemBlocks just before a block is actually set in the world, to allow for adjustments to the
+     * IBlockstate
+     */
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        return this.getDefaultState().withProperty(ACTIVE, false);
+    }
+
+    public boolean hasComparatorInputOverride(IBlockState state)
+    {
+        return true;
+    }
+
+    public int getComparatorInputOverride(IBlockState blockState, World worldIn, BlockPos pos)
+    {
+        return ((Boolean)blockState.getValue(ACTIVE)) ? 15 : 0;
+    }
+
+    /**
+     * Convert the given metadata into a BlockState for this Block
+     */
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return this.getDefaultState().withProperty(ACTIVE, (meta & 4) != 0);
+    }
+
+    /**
+     * Convert the BlockState into the correct metadata value
+     */
+    public int getMetaFromState(IBlockState state)
+    {
+        int i = 0;
+
+        if ((state.getValue(ACTIVE)))
+        {
+            i |= 4;
+        }
+
+        return i;
+    }
+
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, ACTIVE);
+    }
+
+    public boolean isFullCube(IBlockState state)
+    {
+        return true;
+    }
+
+    /**
+     * Get the geometry of the queried face at the given position and state. This is used to decide whether things like
+     * buttons are allowed to be placed on the face, or how glass panes connect to the face, among other things.
+     * <p>
+     * Common values are {@code SOLID}, which is the default, and {@code UNDEFINED}, which represents something that
+     * does not fit the other descriptions and will generally cause other things not to connect to the face.
+     *
+     * @return an approximation of the form of the given face
+     */
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
+    {
+        return face == EnumFacing.DOWN ? BlockFaceShape.SOLID : BlockFaceShape.UNDEFINED;
+    }
+}
--- src/main/java/kobolds/the_elder/items/BookOfBefore.java
+++ src/main/java/kobolds/the_elder/items/BookOfBefore.java
@@ -1,9 +1,20 @@
 package kobolds.the_elder.items;
 
+import kobolds.the_elder.blocks.BlockElderPortalFrame;
+import kobolds.the_elder.init.ModBlocks;
 import kobolds.the_elder.items.base.ItemBase;
-import net.minecraft.item.Item;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 
 import kobolds.the_elder.Elder;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 /*
   BookOfBefore is a book the player can right-click to gain info about the mod
@@ -16,5 +27,147 @@ public class BookOfBefore extends ItemBase {
     public BookOfBefore(String name) {
         super(name);
         this.setCreativeTab(Elder.ELDER_TAB);
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        IBlockState blockState = worldIn.getBlockState(pos);
+        ItemStack itemStack = player.getHeldItem(hand);
+
+
+
+        // If the player can edit here and the core is not active
+        if (player.canPlayerEdit(pos.offset(facing), facing, itemStack) && blockState.getBlock() == ModBlocks.ELDER_PORTAL_CORE && !(blockState.getValue(BlockElderPortalFrame.ACTIVE)))
+        {
+            if (worldIn.isRemote)
+            {
+                return EnumActionResult.SUCCESS;
+            }
+            else
+            {
+                int portalY = pos.getY();
+                int sideLength = Elder.PORTAL_SIZE;
+
+                // starts at SW corner and moves along the ring counter clockwise
+                BlockPos[] corePositions = {
+                        new BlockPos(sideLength / 2 - 1, portalY, sideLength / 2 - 1), // SW
+                        new BlockPos(sideLength / 2 - 1, portalY, sideLength / -2),    // NW
+                        new BlockPos(sideLength / -2, portalY, sideLength / -2),       // NE
+                        new BlockPos(sideLength / -2, portalY, sideLength / 2 - 1)     // SE
+                };
+
+                if (verifyPortalConstruction(worldIn, pos, portalY, corePositions))
+                {
+                    activateBlock(worldIn, blockState, pos);
+
+                    // check if all four cores are activated
+                    if (coreActive(worldIn, corePositions[0]) && coreActive(worldIn, corePositions[1]) && coreActive(worldIn, corePositions[2]) && coreActive(worldIn, corePositions[3])) {
+                        activatePortal(worldIn, corePositions);
+                    }
+                }
+
+                return EnumActionResult.SUCCESS;
+            }
+        }
+
+        return EnumActionResult.FAIL;
+    }
+
+    private boolean coreActive(World worldIn, BlockPos pos) {
+        return worldIn.getBlockState(pos).getValue(BlockElderPortalFrame.ACTIVE);
+    }
+
+    // activate the given block
+    protected void activateBlock(World worldIn,IBlockState blockState, BlockPos pos)
+    {
+        Block block = blockState.getBlock();
+
+        if (block == ModBlocks.ELDER_PORTAL_CORE || block == ModBlocks.ELDER_PORTAL_FRAME) {
+            worldIn.setBlockState(pos, blockState.withProperty(BlockElderPortalFrame.ACTIVE, true), 2);
+            worldIn.updateComparatorOutputLevel(pos, block);
+        }
+    }
+
+    protected void activatePortal(World worldIn, BlockPos[] corePositions) {
+        Block portalBlock = ModBlocks.ELDER_PORTAL;
+
+        // activate frames
+        EnumFacing facing = EnumFacing.NORTH;
+        int numFramesPerSide = Elder.PORTAL_SIZE - 2;
+
+        BlockPos posWalker = corePositions[0];
+        for (BlockPos corePos : corePositions)
+        {
+            posWalker = corePos;
+
+            for (int i=0; i < numFramesPerSide; i++)
+            {
+                // move the walker 1 in the facing direction
+                posWalker = posWalker.offset(facing);
+                // check if the block at the new pos is a frame
+                if (worldIn.getBlockState(posWalker).getBlock() == ModBlocks.ELDER_PORTAL_FRAME)
+                {
+                    activateBlock(worldIn, worldIn.getBlockState(posWalker), posWalker);
+                }
+            }
+            // rotate the current direction clockwise
+            facing = facing.rotateYCCW();
+            System.out.println(facing);
+        }
+
+        System.out.println(facing);
+        System.out.println(posWalker.getX() + ", " + posWalker.getY() + ", " + posWalker.getZ());
+
+        for (int i=0; i<numFramesPerSide*numFramesPerSide; i++)
+        {
+            // rotate
+            if (worldIn.getBlockState(posWalker.offset(facing)).getBlock() == ModBlocks.ELDER_PORTAL_FRAME || worldIn.getBlockState(posWalker.offset(facing)).getBlock() == portalBlock)
+            {
+                facing = facing.rotateYCCW();
+            }
+            // offset
+            posWalker = posWalker.offset(facing);
+            // place
+            worldIn.setBlockState(posWalker, portalBlock.getDefaultState());
+        }
+    }
+
+    protected boolean verifyPortalConstruction(World worldIn, BlockPos pos, int portalY, BlockPos[] corePositions)
+    {
+        int numFramesPerSide = Elder.PORTAL_SIZE - 2;
+
+        EnumFacing facing = EnumFacing.NORTH;
+
+        for (BlockPos corePos : corePositions)
+        {
+            BlockPos posWalker = corePos;
+            // check if the block at the core position is a core
+            if (worldIn.getBlockState(corePos).getBlock() != ModBlocks.ELDER_PORTAL_CORE)
+            {
+                System.out.println("Invalid portal construction - core placement");
+                return false;
+            }
+
+            System.out.println("Valid core placement at: " + corePos.getX() + ", " + corePos.getY() + ", " + corePos.getZ() + " . Checking Frames...");
+
+            // check the frames
+            for (int i=0; i < numFramesPerSide; i++)
+            {
+                // move the walker 1 in the facing direction
+                posWalker = posWalker.offset(facing);
+                // check if the block at the new pos is a frame
+                if (worldIn.getBlockState(posWalker).getBlock() != ModBlocks.ELDER_PORTAL_FRAME)
+                {
+                    System.out.println("Invalid frame placement at: " + posWalker.getX() + ", " + posWalker.getY() + ", " + posWalker.getZ());
+                    return false;
+                }
+                System.out.println("Valid frame at: " + posWalker.getX() + ", " + posWalker.getY() + ", " + posWalker.getZ());
+            }
+            // rotate the current direction clockwise
+            facing = facing.rotateYCCW();
+        }
+        System.out.println("Valid portal construction found.");
+        return true;
     }
 }
